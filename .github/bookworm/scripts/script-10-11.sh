a06c462e2f1ec6634700aacb8f9b571f1e0375c2
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2023-07-13 16:46:59 +0100

    Create script-10-11.sh

---
+++ .github/bookworm/scripts/script-10-11.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Launch Instance
+lxc launch images:debian/bookworm/amd64 tonics-mariadb
+
+# Dependencies
+lxc exec tonics-mariadb -- bash -c "apt update -y && apt upgrade -y"
+
+lxc exec tonics-mariadb -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y mariadb-server"
+
+lxc exec tonics-mariadb -- bash -c "mysql --user=root -sf <<EOS
+-- set root password
+ALTER USER root@localhost IDENTIFIED BY 'tonics_cloud';
+DELETE FROM mysql.user WHERE User='';
+-- delete remote root capabilities
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+-- drop database 'test'
+DROP DATABASE IF EXISTS test;
+-- also make sure there are lingering permissions to it
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+-- make changes immediately
+FLUSH PRIVILEGES;
+EOS
+"
+
+# Clean Debian Cache
+lxc exec tonics-mariadb -- apt clean
+
+# MariaDB Version
+Version=$(lxc exec tonics-mariadb -- mysql -V | awk '{print $5}' | sed 's/,//')
+
+# Publish Image
+mkdir images && lxc stop tonics-mariadb && lxc publish tonics-mariadb --alias tonics-mariadb
+
+# Export Image
+lxc start tonics-mariadb
+lxc image export tonics-mariadb images/mariadb-bookworm-$Version
+
+# Image Info
+lxc image info tonics-mariadb >> images/info.txt
