4896ea3c1ba1513047a17f7ace3d79171f97b788
Olayemi Faruq <37757164+devsrealm@users.noreply.github.com>
2024-04-01 00:22:33 +0100

    Update script-10-11.sh

--- .github/bookworm/scripts/script-10-11.sh
+++ .github/bookworm/scripts/script-10-11.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Init incus
-incus admin init --auto
+sudo incus admin init --auto
 
 # Launch Instance
-lxc launch images:debian/bookworm/amd64 tonics-mariadb
+sudo incus launch images:debian/bookworm/amd64 tonics-mariadb
 
 # Dependencies
-lxc exec tonics-mariadb -- bash -c "apt update -y && apt upgrade -y"
+sudo incus exec tonics-mariadb -- bash -c "apt update -y && apt upgrade -y"
 
-lxc exec tonics-mariadb -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y mariadb-server"
+sudo incus exec tonics-mariadb -- bash -c "DEBIAN_FRONTEND=noninteractive apt install -y mariadb-server"
 
-lxc exec tonics-mariadb -- bash -c "mysql --user=root -sf <<EOS
+sudo incus exec tonics-mariadb -- bash -c "mysql --user=root -sf <<EOS
 -- set root password
 ALTER USER root@localhost IDENTIFIED BY 'tonics_cloud';
 DELETE FROM mysql.user WHERE User='';
@@ -27,17 +27,17 @@ EOS
 "
 
 # Clean Debian Cache
-lxc exec tonics-mariadb -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
+sudo incus exec tonics-mariadb -- bash -c "apt clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*"
 
 # MariaDB Version
-Version=$(lxc exec tonics-mariadb -- mysql -V | awk '{print $5}' | sed 's/,//')
+Version=$(sudo incus exec tonics-mariadb -- mysql -V | awk '{print $5}' | sed 's/,//')
 
 # Publish Image
-mkdir images && lxc stop tonics-mariadb && lxc publish tonics-mariadb --alias tonics-mariadb
+mkdir images && sudo incus stop tonics-mariadb && sudo incus publish tonics-mariadb --alias tonics-mariadb
 
 # Export Image
-lxc start tonics-mariadb
-lxc image export tonics-mariadb images/mariadb-bookworm-$Version
+sudo incus start tonics-mariadb
+sudo incus image export tonics-mariadb images/mariadb-bookworm-$Version
 
 # Image Info
-lxc image info tonics-mariadb >> images/info.txt
+sudo incus image info tonics-mariadb >> images/info.txt
